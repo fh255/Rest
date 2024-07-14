10963ff4bda1f627ba7dab6ff38564349f0f1261
Fatema Mahabub <fmahabub8@gmail.com>
2024-07-14 09:14:06 +0000

    image filter correction

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
 
-
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
