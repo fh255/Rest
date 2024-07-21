da8e9e070c4f8843d92f9a19fc13e90ef243ca69
Fatema Mahabub <fmahabub8@gmail.com>
2024-07-21 14:10:07 +0000

    added condition for local and production database url load

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 # import env 
-# import dj_database_url
+import dj_database_url
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -119,15 +119,18 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    )
-}
+# Configure the default database
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
